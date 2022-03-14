a5ebdbaa85e9174275ae09e599016ad3c1165b18
Matthew Tramantano <matt.tramantano@gmail.com>
2022-03-14 13:29:24 -0600

    fixed file safety

--- src/styles/types.ts
+++ src/styles/types.ts
@@ -1,6 +1,8 @@
 // Generics here may not work with all the type checking and specific json tags
-export type ThemeSON = {[string:string]: string}
-export type ProjectTheme = {
-    colors: ThemeSON;
-    container: ThemeSON;
-}
+// export type ThemeSON = {[string:string]: string}
+// export type ProjectTheme = {
+//     colors: ThemeSON;
+//     container: ThemeSON;
+// }
+
+export default {}
