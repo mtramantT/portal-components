c44cb1c961ce2e84efe03304fff61166ef1bec2c
Matthew Tramantano <matt.tramantano@gmail.com>
2022-03-18 16:04:08 -0600

    added example to app

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,22 @@
 import React from 'react';
-import { Column, Container, Row } from './layout/rwd';
+import { Col, Grid, Row } from './layout/flex';
 
 
 function App() {
   return (
     <div className="App">
-      <Container>
+      <Grid>
         <Row>
-          <Column>gfghfjhfjhfh</Column>
-          <Column>kkkkkkkkkkkkkk</Column>
+          <Col stack="sm" size={1}>gfghfjhfjhfh</Col>
+          <Col stack="sm" size={1}>kkkkkkkkkkkkkk</Col>
+          <Col stack="sm">gggggggggggg</Col>
         </Row>
-      </Container>
+        <Row style={{padding: '50px'}}>
+          <Col size={1} stack="sm"></Col>
+          <Col size={1} stack="sm">kkkkkkkkkkkkkk</Col>
+          <Col stack="sm"></Col>
+        </Row>
+      </Grid>
     </div>
   );
 }
