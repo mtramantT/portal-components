e586dde3dad7105d1575cc1c2d242fbc494e286d
Matthew Tramantano <matt.tramantano@gmail.com>
2022-03-25 12:33:52 -0600

    added value managment to input

--- src/components/form/input.tsx
+++ src/components/form/input.tsx
@@ -1,38 +1,59 @@
 import * as React from 'react';
 import styled from 'styled-components';
 
-export interface InputProps extends Partial<PartialProps> {
-
+export interface InputProps extends React.HTMLAttributes<HTMLInputElement>, Partial<PartialProps> {
+    id: string;
 }
 
 interface PartialProps {
-    onFocus: (event: React.FocusEvent<HTMLInputElement>) => void;
-    onBlur: (event: React.FocusEvent<HTMLInputElement>) => void;
+    label: string;
+    required: boolean;
 }
 
 const Container = styled.div``;
-const Label = styled.div``;
+const Label = styled.label``;
 const Input = styled.input``;
 
 export const input: React.FC<InputProps & React.HTMLProps<HTMLInputElement>> = (props: InputProps) => {
+    const [value, setValue] = React.useState<string>('');
     const [focus, setFocus] = React.useState<boolean>(false);
-    const [hasValue, setHasValue] = React.useState<boolean>(false)
+    const [hasValue, setHasValue] = React.useState<boolean>(false);
 
-    const onFocus = React.useCallback((event: React.FocusEvent<HTMLInputElement>) => {
+    React.useEffect(() => {
+        setHasValue(focus || value.length > 0)
+    }, [value, focus])
+
+    React.useEffect(() => {
+        if (focus === false && value.trim() === '') {
+            setValue(value.trim());
+            setHasValue(false);
+        }
+    },[focus])
+
+    const handleChange = React.useCallback((event: React.ChangeEvent<HTMLInputElement>) => {
+        setValue(event.target.value);
+        props.onChange && props.onChange(event);
+    },[])
+
+    const handleFocus = React.useCallback((event: React.FocusEvent<HTMLInputElement>) => {
         setFocus(true);
         props.onFocus && props.onFocus(event);
     },[props.onFocus]);
 
-    const onBlur = React.useCallback((event: React.FocusEvent<HTMLInputElement>) => {
+    const handleBlur = React.useCallback((event: React.FocusEvent<HTMLInputElement>) => {
         setFocus(false);
         props.onBlur && props.onBlur(event);
     }, [props.onBlur]);
 
     return (
         <Container>
-            <Label>
-                <Input />
-            </Label>
+            { props.label && <Label htmlFor={props.id}>{props.label}</Label>}
+            <Input 
+                id={props.id}
+                name={props.id}
+                onFocus={handleFocus}
+                onBlur={handleBlur} 
+                onChange={handleChange}/>
         </Container>
     );
 }
