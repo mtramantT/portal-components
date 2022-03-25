85f0d106e31ef37f9e4ace8add45d143b68f7e5d
Matthew Tramantano <matt.tramantano@gmail.com>
2022-03-25 12:51:45 -0600

    finished added remaining parts to input. some remaining tasks to resolve still

--- src/components/form/input.tsx
+++ src/components/form/input.tsx
@@ -10,9 +10,30 @@ interface PartialProps {
     required: boolean;
 }
 
-const Container = styled.div``;
-const Label = styled.label``;
-const Input = styled.input``;
+const Label = styled.label<{ focus: boolean, hasValue: boolean}>`
+    position: absolute;
+    transition: 230;
+    margin-bottom: 3px;
+    bottom: ${({ hasValue }) => hasValue ? '20px' : '0px'};
+    font-size: ${({ hasValue }) => hasValue ? '14px' : '16px'};
+    color: ${({focus}) => focus ? 'blue' : 'black'};
+`;
+
+const Container = styled.div<{}>`
+    display: inline-block;
+    position: relative;
+    margin: 15px;
+    width: 95%;
+`;
+
+const Input = styled.input`
+    border-width: 0 0 1px 0;
+    font-size: 16px;
+    width: 100%;
+    &:focus {
+        outline: 1px solid blue;
+    }
+`;
 
 export const input: React.FC<InputProps & React.HTMLProps<HTMLInputElement>> = (props: InputProps) => {
     const [value, setValue] = React.useState<string>('');
@@ -46,11 +67,12 @@ export const input: React.FC<InputProps & React.HTMLProps<HTMLInputElement>> = (
     }, [props.onBlur]);
 
     return (
-        <Container>
+        <Container className={'input-wrapper-' + props.id }>
             { props.label && <Label htmlFor={props.id}>{props.label}</Label>}
             <Input 
                 id={props.id}
                 name={props.id}
+                className={props.className || props.id}
                 onFocus={handleFocus}
                 onBlur={handleBlur} 
                 onChange={handleChange}/>
