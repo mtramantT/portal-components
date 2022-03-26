bf07c5d1f6765dbe240394da319d527f8da6f972
Matthew Tramantano <matt.tramantano@gmail.com>
2022-03-26 00:19:44 -0600

    fixed typeguards in flex

--- src/layout/flex/types.ts
+++ src/layout/flex/types.ts
@@ -9,11 +9,13 @@ export const isDeviceSize = (test: any): test is DeviceSize => {
 // Media Query
 export type MediaQuery = {deviceSize: DeviceSize; styles: string[]}
 export const isMediaQuery = (test: any): test is MediaQuery => {
+    if (typeof test !== 'object') return false
     return ("deviceSize" in test && "styles" in test);
 }
 
 // Custom Media
 export type CustomMediaQuery = {styles: string|string[]; mediaQuery: string;}
 export const isCustomMedia = (test: any): test is {styles: string|string[]; mediaQuery: string;} => {
+    if (typeof test !== 'object') return false
     return ("styles" in test && "mediaQuery" in test);
 }
