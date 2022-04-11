ad829a5e241f65596d7abdc12576e148b851612c
Matthew Tramantano <matt.tramantano@gmail.com>
2022-04-11 08:31:59 -0600

    added index

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Col, Grid, Row } from './layout/flex';
+import { Input } from './components/form/input'
 
 
 function App() {
@@ -11,6 +12,9 @@ function App() {
           <Col size={1}>kkkkkkkkkkkkkk</Col>
           <Col size={3}>gggggggggggg</Col>
         </Row>
+        <div className='test-form'>
+          <Input id='test-input' label='Test Input:'/>
+        </div>
       </Grid>
     </div>
   );
