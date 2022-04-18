841b78d26f16e752d549c57404ed99fdf60a558e
Matthew Tramantano <matt.tramantano@gmail.com>
2022-04-17 22:23:20 -0600

    started stepper

---
+++ src/components/stepper/stepper.tsx
@@ -0,0 +1,31 @@
+/**
+ * Form Stepper
+ * Takes a number of "steps" with a "canProcceed" func 
+ * Bottom button layout should resemble something like: [prev, next, submit/complete]
+ * More buttons: Undo, Undo All, Undo Page
+ */
+import React from "react";
+import styled from "styled-components";
+
+const Container = styled.div``;
+
+interface StepperProps {
+    steps: any[];
+    onSubmit: () => void;
+}
+
+
+export const Stepper = (props: StepperProps) => {
+    const [currentStep, setCurrentStep] = React.useState(0);
+    const [completedSteps, setCompletedSteps] = React.useState<{[id: number]: boolean}>({})
+
+    return (
+        <Container>
+
+        </Container>
+    );
+}
+
+export const Step = (props: {}) => {
+    return null;
+}
